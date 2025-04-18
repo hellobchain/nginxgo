097b9d3f34c1515d1b406ce4feac3b0dfb632b87
wsw365904 <wangshouwei180@163.com>
2025-04-18 20:18:10 +0800

    update

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # nginxgo
+
  go语言实现nginx
 
 ---
+
 ## 使用流程
+
 - 按照规范填写配置文件（在`nginxgo`目录下的config目录中），规范见 [config规范](#config规范)
-- 运行`nginxgo`。
+- 运行`nginxgo -c`。
 - 以下为命令：
+
 1. `start`--启动服务
 2. `reset`--服务热重启
 3. `stop`--停止服务
 
 ## config规范
+
 config分为[server]、[location]、[upstream]三个模块。server模块与端口绑定，统辖location块。location块用于提供具体的服务，例如负载均衡或者文件服务。upstream块为后端服务器池。
 
 - `[server]`-用于标示server块的开始
@@ -29,12 +34,9 @@ config分为[server]、[location]、[upstream]三个模块。server模块与端�
 
   upstream块下的字段有：
 - - `name`-必须紧跟（不包括注释）在`[upstream]`标识符后。用于命名该后端服务器池。
+- - `proxy_set_header`-用于设置请求头部的字段。可以设置多个。
 - - `schema`-用于指定后端服务器的协议。
-- -  `replicas`-每个真实服务器节点对应的虚拟节点数，用于哈希一致性算法的建构。
+- - `replicas`-每个真实服务器节点对应的虚拟节点数，用于哈希一致性算法的建构。
 - - `name`字段之后跟随后端服务器的ip地址
 
 - `[end]`-用于标示每个块的结束。
-
-## 一些特性
-- 服务热重启：热重启不会影响到整个服务。只会影响对应被修改的区块。
-- 自动删除失效的后端服务器：如果客户端请求一个后端服务器三次之后该服务器仍未响应，那么将从该后端服务器池中剔除这一服务器。
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"strconv"
 	"strings"
 	"sync"
+	"syscall"
 
 	"github.com/hellobchain/nginxgo/common/constant"
 	"github.com/hellobchain/nginxgo/nginxgo"
@@ -12,11 +16,41 @@ import (
 )
 
 var logger = wlogging.MustGetLoggerWithoutName()
-
+var engine *nginxgo.Engine // 声明一个 Engine 变量
+var mu sync.Mutex          // 互斥锁，用于保护 engine 变量的访问
 func main() {
-	var engine *nginxgo.Engine // 声明一个 Engine 变量
-	var mu sync.Mutex          // 互斥锁，用于保护 engine 变量的访问
+	// 监听SIGHUP信号
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGHUP)
 	command = strings.TrimSpace(command)
+	operateCMD(command, 1)
+	utils.HandleSignals(logger)
+	pidStr := strconv.Itoa(os.Getpid())
+	os.WriteFile("nginxgo.pid", []byte(pidStr), 0644)
+	for {
+		select {
+		case <-signalChan:
+			// 收到 SIGHUP 信号，执行重置操作
+			logger.Info("nginxgo: received SIGHUP signal, resetting engine")
+			operateCMD(constant.CMD_RESET, -1)
+		}
+	}
+}
+
+var command string
+var flags *pflag.FlagSet
+
+const flagCommand = "commond"
+
+func init() {
+	flags = &pflag.FlagSet{}
+	flags.StringVar(&command, flagCommand, "start", "操作命令,--command=start 启动服务,--command=stop 停止服务,--command=reset 重置服务,--command=help 查看帮助")
+	// 定义一个简写的参数
+	pflag.StringVarP(&command, "c", "c", "start", "操作命令,--command=start 启动服务,--command=stop 停止服务,--command=reset 重置服务,--command=help 查看帮助")
+	pflag.Parse()
+}
+
+func operateCMD(command string, pid int) {
 	switch command {
 	case constant.CMD_START:
 		mu.Lock()
@@ -28,43 +62,91 @@ func main() {
 		}
 		mu.Unlock()
 	case constant.CMD_RESET:
-		mu.Lock()
-		if engine != nil {
-			engine.Reset()
+		if pid > 0 {
+			pid, err := readPidFile()
+			if err != nil {
+				logger.Error("nginxgo: read pid file error:", err)
+			} else {
+				sendSignalHup(pid)
+			}
+			os.Exit(1)
 		} else {
-			logger.Warn("nginxgo: engine not started yet")
+			mu.Lock()
+			if engine != nil {
+				engine.Reset()
+			} else {
+				logger.Warn("nginxgo: engine not started yet")
+			}
+			mu.Unlock()
 		}
-		mu.Unlock()
 	case constant.CMD_STOP:
-		mu.Lock()
-		if engine != nil {
-			engine.Stop()
-			engine = nil // 释放引用，方便重新启动
+		if pid > 0 {
+			pid, err := readPidFile()
+			if err != nil {
+				logger.Error("nginxgo: read pid file error:", err)
+			} else {
+				sendSignalTerm(pid)
+			}
+			os.Exit(1)
 		} else {
-			logger.Warn("nginxgo: engine not started yet")
+			mu.Lock()
+			if engine != nil {
+				engine.Stop()
+				engine = nil // 释放引用，方便重新启动
+			} else {
+				logger.Warn("nginxgo: engine not started yet")
+			}
+			mu.Unlock()
 		}
-		mu.Unlock()
 	case constant.CMD_HELP:
-		logger.Info("nginxgo: help")
-		logger.Info("nginxgo: start")
-		logger.Info("nginxgo: stop")
-		logger.Info("nginxgo: reset")
-		logger.Info("nginxgo: help")
+		logger.Info("nginxgo -c=help")
+		logger.Info("nginxgo -c=start")
+		logger.Info("nginxgo -c=stop")
+		logger.Info("nginxgo -c=reset")
 	default:
 		logger.Error("nginxgo: error command")
 	}
-	utils.HandleSignals(logger)
-	for {
+}
+
+func sendSignalHup(targetPID int) {
+	// 获取目标进程
+	process, err := os.FindProcess(targetPID)
+	if err != nil {
+		logger.Error("Failed to find process:", err)
+		return
+	}
+	// 向目标进程发送 SIGHUP 信号
+	err = process.Signal(syscall.SIGHUP)
+	if err != nil {
+		logger.Error("Failed to send SIGHUP signal:", err)
+		return
 	}
 }
 
-var command string
-var flags *pflag.FlagSet
+func sendSignalTerm(targetPID int) {
+	// 获取目标进程
+	process, err := os.FindProcess(targetPID)
+	if err != nil {
+		logger.Error("Failed to find process:", err)
+		return
+	}
+	// 向目标进程发送 SIGTERM 信号
+	err = process.Signal(syscall.SIGTERM)
+	if err != nil {
+		logger.Error("Failed to send SIGTERM signal:", err)
+		return
+	}
+}
 
-const flagCommand = "commond"
-
-func init() {
-	flags = &pflag.FlagSet{}
-	flags.StringVar(&command, flagCommand, "start",
-		"操作命令,--command=start 启动服务,--command=stop 停止服务,--command=reset 重置服务,--command=help 查看帮助")
+func readPidFile() (int, error) {
+	data, err := os.ReadFile("nginxgo.pid")
+	if err != nil {
+		return -1, err
+	}
+	pidStr := strings.TrimSpace(string(data))
+	pid, err := strconv.Atoi(pidStr)
+	if err != nil {
+		return -1, err
+	}
+	return pid, nil
 }
